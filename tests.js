10af40c62f930f0a317d6e324a5a2b84f76835b2
Space <space@471.no>
2020-11-19 18:10:48 +0100

    Add dark/light themes, code output formatting improvements

--- tests.js
+++ tests.js
@@ -1,5 +1,17 @@
+function tests() {
+	it('should pass, but with dependencies', function() {
+		assert(1 === 1);
+	});
+
+	it('should fail, but with dependencies', function() {
+		assert(1 !== 1);
+	});
+}
+
+loadScriptsAndRunTests(["your-code-for-testing.js"], tests);
+
 it('should pass', function() {
-	assert(1 == 1);
+	assert(1 === 1);
 });
 
 it('should fail', function() {
